bd8f4a432ec4f5e5b333309f1c393dc823591725
Alex Meredith <alexm0802@gmail.com>
2022-06-13 16:01:19 -0400

    fixed name of project for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 
 # -- Project information -----------------------------------------------------
 
-project = "SM Thesis"
+project = "focal-loss-pytorch"
 copyright = '2022, Alex Meredith'
 author = 'Alex Meredith'
 
